e04c9b0d89ff2acbff1274d71e3dc5aaeff170ac
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2024-09-25 16:08:02 +0530

    Update FrequentlyAskedQuestions.java

--- FrequentlyAskedQuestions.java
+++ FrequentlyAskedQuestions.java
@@ -1,3 +1,3 @@
 1) How did you implement security in your application ?
-2) What are features of Java 17 ?
+2) What are features of Java 17 ? sealed class 
 3) Can we override static methods in Java ? No. 
