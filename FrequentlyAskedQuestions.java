2f4ec62d51c351e67ada7d5f3dbe23188a78722b
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2024-09-25 16:30:44 +0530

    Update FrequentlyAskedQuestions.java

--- FrequentlyAskedQuestions.java
+++ FrequentlyAskedQuestions.java
@@ -1,3 +1,7 @@
 1) How did you implement security in your application ?
 2) What are features of Java 17 ? sealed class 
 3) Can we override static methods in Java ? No. 
+4) Why String is immutable ? 
+  String is often used as database passwords and usernames etc. So it needs to be immutable to prevent modification .
+  It saves lot of space in string pool of heap by avoiding duplicate creation
+  It can be safely used in multithreaded environments.
